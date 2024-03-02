2347de48ce3915b1f4f58f652c3003219e1a80ac
“[firstname <rabya759@gmail.com>
2024-03-03 01:37:35 +0500

    third commit

---
+++ Assignment_04/main.ts
@@ -0,0 +1,6 @@
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+
+
+let pname : boolean = 2+2
+console.log(pname)
