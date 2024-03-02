b74c165caaf924a3ba26db0ce8eec2b5aad3a0bd
“[firstname <rabya759@gmail.com>
2024-03-03 01:46:38 +0500

    forth commit

--- Assignment_04/main.ts
+++ Assignment_04/main.ts
@@ -1,3 +1,5 @@
+//Assignment no 04 
+
 // Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 
 
