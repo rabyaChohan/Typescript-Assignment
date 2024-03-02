b7c602400f6c8cc210e6143cf4efc7d2d4b374ab
“[firstname <rabya759@gmail.com>
2024-03-03 00:40:12 +0500

    second commit

---
+++ Assignment_03/main.ts
@@ -0,0 +1,7 @@
+// Assignment No 03
+
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string = "Rabya Chohan"
+
+console.log()
