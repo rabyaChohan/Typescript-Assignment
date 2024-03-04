c359139de0e7e05b77b38692d43695abc423d361
“[firstname <rabya759@gmail.com>
2024-03-04 22:31:44 +0500

    second commit

--- Assignment_05/main.ts
+++ Assignment_05/main.ts
@@ -1,3 +1,7 @@
 //Assignment no 05
 // Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
 
+
+let message : string = '"The future belongs to those who believe in the beauty of their dreams."'
+let famous_person : String = "Eleanor Roosevelt once said,"
+console.log(famous_person , message)
