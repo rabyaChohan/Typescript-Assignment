6bac8dac4e24bff64d28ff279e7007231306d9c9
“[firstname <rabya759@gmail.com>
2024-03-04 22:43:01 +0500

    third commit

--- Assignment_05/main.js
+++ Assignment_05/main.js
@@ -1,1 +1,5 @@
-console.log(2 + 2 + 3 + 4 + 3 + 22);
+//Assignment no 05
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var message = '"The future belongs to those who believe in the beauty of their dreams."';
+var famous_person = "Eleanor Roosevelt once said,";
+console.log(famous_person, message);
