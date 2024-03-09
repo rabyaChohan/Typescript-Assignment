b534a39bdb4b699d914b9dc1e6cf009983fadfdc
“[firstname <rabya759@gmail.com>
2024-03-10 00:11:32 +0500

    Update

---
+++ Assignment_09/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Assignment no 09
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+let myFavnumber = 9;
+let message = "My Favorite Number is " + myFavnumber;
+console.log(message);
---
+++ Assignment_09/main.ts
@@ -0,0 +1,9 @@
+// Assignment no 09
+
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+
+let myFavnumber : number = 9
+let message : string = "My Favorite Number is " + myFavnumber
+
+console.log (message)
