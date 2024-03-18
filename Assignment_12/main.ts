8787319378654423815cd727900ae82bab024c7d
“[firstname <rabya759@gmail.com>
2024-03-19 00:54:29 +0500

    Update

---
+++ Assignment_12/main.ts
@@ -0,0 +1,9 @@
+// Assignment no 12 
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names : string[]= ["Rabya", "Muneeba" , "Sumaiya" , "Kauser", "Areeba" ] 
+let message : string = "Hello "
+for(let i=0 ; i <names.length; i++)
+{ 
+    console.log(message+names[i]) 
+}
