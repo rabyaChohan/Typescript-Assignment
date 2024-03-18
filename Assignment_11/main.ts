a9f02fc2e62083a399fca6f2560b8129c63862b4
“[firstname <rabya759@gmail.com>
2024-03-19 00:49:38 +0500

    Update

--- Assignment_11/main.ts
+++ Assignment_11/main.ts
@@ -1,4 +1,5 @@
-// Assignment no 11 // Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time. 
+// Assignment no 11 
+// Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time. 
 let names : string[]= ["Rabya", "Muneeba" , "Sumaiya" , "Kauser", "Areeba" ] 
 for(let i=0 ; i <names.length; i++)
 { 
